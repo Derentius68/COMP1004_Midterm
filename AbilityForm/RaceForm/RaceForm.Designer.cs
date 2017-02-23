634dfe39a0311425fd926889437d8d255ca20bd2
Derentius68 <asbywater@gmail.com>
2017-02-23 17:49:09 -0500

    Added Race ability scores

--- AbilityForm/RaceForm/RaceForm.Designer.cs
+++ AbilityForm/RaceForm/RaceForm.Designer.cs
@@ -30,19 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RaceForm));
             this.racesGroupBox = new System.Windows.Forms.GroupBox();
-            this.humanRadioButton = new System.Windows.Forms.RadioButton();
-            this.dwarfRadioButton = new System.Windows.Forms.RadioButton();
-            this.elfRadioButton = new System.Windows.Forms.RadioButton();
-            this.halflingRadioButton = new System.Windows.Forms.RadioButton();
-            this.tieflingRadioButton = new System.Windows.Forms.RadioButton();
+            this.racialBonusTextBox = new System.Windows.Forms.TextBox();
             this.aasimarRadioButton = new System.Windows.Forms.RadioButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tieflingRadioButton = new System.Windows.Forms.RadioButton();
+            this.halflingRadioButton = new System.Windows.Forms.RadioButton();
+            this.elfRadioButton = new System.Windows.Forms.RadioButton();
+            this.dwarfRadioButton = new System.Windows.Forms.RadioButton();
+            this.humanRadioButton = new System.Windows.Forms.RadioButton();
+            this.characterPictureBox = new System.Windows.Forms.PictureBox();
+            this.nextButton = new System.Windows.Forms.Button();
             this.racesGroupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // racesGroupBox
             // 
+            this.racesGroupBox.Controls.Add(this.racialBonusTextBox);
             this.racesGroupBox.Controls.Add(this.aasimarRadioButton);
             this.racesGroupBox.Controls.Add(this.tieflingRadioButton);
             this.racesGroupBox.Controls.Add(this.halflingRadioButton);
@@ -51,10 +54,71 @@
             this.racesGroupBox.Controls.Add(this.humanRadioButton);
             this.racesGroupBox.Location = new System.Drawing.Point(12, 12);
             this.racesGroupBox.Name = "racesGroupBox";
-            this.racesGroupBox.Size = new System.Drawing.Size(250, 237);
+            this.racesGroupBox.Size = new System.Drawing.Size(173, 237);
             this.racesGroupBox.TabIndex = 0;
             this.racesGroupBox.TabStop = false;
             this.racesGroupBox.Text = "Races";
+            // 
+            // racialBonusTextBox
+            // 
+            this.racialBonusTextBox.Location = new System.Drawing.Point(7, 174);
+            this.racialBonusTextBox.Multiline = true;
+            this.racialBonusTextBox.Name = "racialBonusTextBox";
+            this.racialBonusTextBox.Size = new System.Drawing.Size(166, 57);
+            this.racialBonusTextBox.TabIndex = 6;
+            // 
+            // aasimarRadioButton
+            // 
+            this.aasimarRadioButton.Enabled = false;
+            this.aasimarRadioButton.Location = new System.Drawing.Point(6, 139);
+            this.aasimarRadioButton.Name = "aasimarRadioButton";
+            this.aasimarRadioButton.Size = new System.Drawing.Size(104, 24);
+            this.aasimarRadioButton.TabIndex = 7;
+            this.aasimarRadioButton.Text = "Aasimar";
+            // 
+            // tieflingRadioButton
+            // 
+            this.tieflingRadioButton.AutoSize = true;
+            this.tieflingRadioButton.Location = new System.Drawing.Point(7, 116);
+            this.tieflingRadioButton.Name = "tieflingRadioButton";
+            this.tieflingRadioButton.Size = new System.Drawing.Size(59, 17);
+            this.tieflingRadioButton.TabIndex = 4;
+            this.tieflingRadioButton.Text = "Tiefling";
+            this.tieflingRadioButton.UseVisualStyleBackColor = true;
+            this.tieflingRadioButton.CheckedChanged += new System.EventHandler(this.tieflingRadioButton_CheckedChanged);
+            // 
+            // halflingRadioButton
+            // 
+            this.halflingRadioButton.AutoSize = true;
+            this.halflingRadioButton.Location = new System.Drawing.Point(7, 92);
+            this.halflingRadioButton.Name = "halflingRadioButton";
+            this.halflingRadioButton.Size = new System.Drawing.Size(60, 17);
+            this.halflingRadioButton.TabIndex = 3;
+            this.halflingRadioButton.Text = "Halfling";
+            this.halflingRadioButton.UseVisualStyleBackColor = true;
+            this.halflingRadioButton.CheckedChanged += new System.EventHandler(this.halflingRadioButton_CheckedChanged);
+            // 
+            // elfRadioButton
+            // 
+            this.elfRadioButton.AutoSize = true;
+            this.elfRadioButton.Location = new System.Drawing.Point(7, 68);
+            this.elfRadioButton.Name = "elfRadioButton";
+            this.elfRadioButton.Size = new System.Drawing.Size(37, 17);
+            this.elfRadioButton.TabIndex = 2;
+            this.elfRadioButton.Text = "Elf";
+            this.elfRadioButton.UseVisualStyleBackColor = true;
+            this.elfRadioButton.CheckedChanged += new System.EventHandler(this.elfRadioButton_CheckedChanged);
+            // 
+            // dwarfRadioButton
+            // 
+            this.dwarfRadioButton.AutoSize = true;
+            this.dwarfRadioButton.Location = new System.Drawing.Point(7, 44);
+            this.dwarfRadioButton.Name = "dwarfRadioButton";
+            this.dwarfRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.dwarfRadioButton.TabIndex = 1;
+            this.dwarfRadioButton.Text = "Dwarf";
+            this.dwarfRadioButton.UseVisualStyleBackColor = true;
+            this.dwarfRadioButton.CheckedChanged += new System.EventHandler(this.dwarfRadioButton_CheckedChanged);
             // 
             // humanRadioButton
             // 
@@ -67,81 +131,44 @@
             this.humanRadioButton.TabStop = true;
             this.humanRadioButton.Text = "Human";
             this.humanRadioButton.UseVisualStyleBackColor = true;
+            this.humanRadioButton.CheckedChanged += new System.EventHandler(this.humanRadioButton_CheckedChanged);
             // 
-            // dwarfRadioButton
+            // characterPictureBox
             // 
-            this.dwarfRadioButton.AutoSize = true;
-            this.dwarfRadioButton.Location = new System.Drawing.Point(7, 44);
-            this.dwarfRadioButton.Name = "dwarfRadioButton";
-            this.dwarfRadioButton.Size = new System.Drawing.Size(53, 17);
-            this.dwarfRadioButton.TabIndex = 1;
-            this.dwarfRadioButton.Text = "Dwarf";
-            this.dwarfRadioButton.UseVisualStyleBackColor = true;
+            this.characterPictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("characterPictureBox.ErrorImage")));
+            this.characterPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("characterPictureBox.Image")));
+            this.characterPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("characterPictureBox.InitialImage")));
+            this.characterPictureBox.Location = new System.Drawing.Point(191, 12);
+            this.characterPictureBox.Name = "characterPictureBox";
+            this.characterPictureBox.Size = new System.Drawing.Size(271, 273);
+            this.characterPictureBox.TabIndex = 6;
+            this.characterPictureBox.TabStop = false;
+            this.characterPictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // elfRadioButton
+            // nextButton
             // 
-            this.elfRadioButton.AutoSize = true;
-            this.elfRadioButton.Location = new System.Drawing.Point(7, 68);
-            this.elfRadioButton.Name = "elfRadioButton";
-            this.elfRadioButton.Size = new System.Drawing.Size(37, 17);
-            this.elfRadioButton.TabIndex = 2;
-            this.elfRadioButton.Text = "Elf";
-            this.elfRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // halflingRadioButton
-            // 
-            this.halflingRadioButton.AutoSize = true;
-            this.halflingRadioButton.Location = new System.Drawing.Point(7, 92);
-            this.halflingRadioButton.Name = "halflingRadioButton";
-            this.halflingRadioButton.Size = new System.Drawing.Size(60, 17);
-            this.halflingRadioButton.TabIndex = 3;
-            this.halflingRadioButton.Text = "Halfling";
-            this.halflingRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // tieflingRadioButton
-            // 
-            this.tieflingRadioButton.AutoSize = true;
-            this.tieflingRadioButton.Location = new System.Drawing.Point(7, 116);
-            this.tieflingRadioButton.Name = "tieflingRadioButton";
-            this.tieflingRadioButton.Size = new System.Drawing.Size(59, 17);
-            this.tieflingRadioButton.TabIndex = 4;
-            this.tieflingRadioButton.Text = "Tiefling";
-            this.tieflingRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // aasimarRadioButton
-            // 
-            this.aasimarRadioButton.AutoSize = true;
-            this.aasimarRadioButton.Location = new System.Drawing.Point(7, 140);
-            this.aasimarRadioButton.Name = "aasimarRadioButton";
-            this.aasimarRadioButton.Size = new System.Drawing.Size(62, 17);
-            this.aasimarRadioButton.TabIndex = 5;
-            this.aasimarRadioButton.Text = "Aasimar";
-            this.aasimarRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(191, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(271, 273);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.nextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.nextButton.Location = new System.Drawing.Point(42, 275);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(103, 42);
+            this.nextButton.TabIndex = 15;
+            this.nextButton.Text = "Next Sheet";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
             // RaceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(474, 348);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.characterPictureBox);
             this.Controls.Add(this.racesGroupBox);
             this.Name = "RaceForm";
             this.Text = "Races";
             this.racesGroupBox.ResumeLayout(false);
             this.racesGroupBox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -149,13 +176,15 @@
         #endregion
 
         private System.Windows.Forms.GroupBox racesGroupBox;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox characterPictureBox;
         private System.Windows.Forms.RadioButton aasimarRadioButton;
         private System.Windows.Forms.RadioButton tieflingRadioButton;
         private System.Windows.Forms.RadioButton halflingRadioButton;
         private System.Windows.Forms.RadioButton elfRadioButton;
         private System.Windows.Forms.RadioButton dwarfRadioButton;
         private System.Windows.Forms.RadioButton humanRadioButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox racialBonusTextBox;
     }
 }
 
--- AbilityForm/RaceForm/RaceForm.cs
+++ AbilityForm/RaceForm/RaceForm.cs
@@ -12,14 +12,135 @@ namespace RaceForm
 {
     public partial class RaceForm : Form
     {
+        public AbilityForm previousForm { get; set; }
+
+        //Private Variables
+        private string _selectedRace;
+
+        private int _strengthMod = Convert.ToInt32(Program.character.Strength);
+        private int _dexterityMod = Convert.ToInt32(Program.character.Dexterity);
+        private int _constitutionMod = Convert.ToInt32(Program.character.Constitution);
+        private int _intelligenceMod = Convert.ToInt32(Program.character.Intelligence);
+        private int _wisdomMod = Convert.ToInt32(Program.character.Wisdom);
+        private int _charismaMod = Convert.ToInt32(Program.character.Charisma);
+
         public RaceForm()
         {
             InitializeComponent();
+        }
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            Character character = Program.character;
+
+            character.Race = _selectedRace;
+
+            character.Strength = _strengthMod.ToString();
+            character.Dexterity = _dexterityMod.ToString();
+            character.Constitution = _constitutionMod.ToString();
+            character.Intelligence = _intelligenceMod.ToString();
+            character.Wisdom = _wisdomMod.ToString();
+            character.Charisma = _charismaMod.ToString();
+
+            JobForm jobform = new JobForm();
+            jobform.previousForm = this;
+
+            jobform.Show();
+            this.Hide();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
+
+        //Radio Button for Human
+        private void humanRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.M_Human1;
+            RadioButton selectedRace = (RadioButton)sender;
+
+            racialBonusTextBox.Text = "Increase all ability scores by 1";
+
+            _strengthMod = _strengthMod + 1;
+            _dexterityMod = _dexterityMod + 1;
+            _constitutionMod = _constitutionMod + 1;
+            _intelligenceMod = _intelligenceMod + 1;
+            _wisdomMod = _wisdomMod + 1;
+            _charismaMod = _charismaMod + 1;
+
+            this._selectedRace = selectedRace.Text;
+        }
+
+        //Radio Button for Dwarf
+        private void dwarfRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.M_Dwarf1;
+            RadioButton selectedRace = (RadioButton)sender;
+            racialBonusTextBox.Text = "Increase Constitution by 2, Strength by 1, and Wisdom by 1";
+
+            _strengthMod = _strengthMod + 1;
+            _constitutionMod = _constitutionMod + 2;
+            _wisdomMod = _wisdomMod + 1;
+
+            this._selectedRace = selectedRace.Text;
+        }
+
+        //Radio Button for Tiefling (Half-Fiend, Half-Human)
+        private void tieflingRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.M_Tiefling1;
+            RadioButton selectedRace = (RadioButton)sender;
+            racialBonusTextBox.Text = "Increase Charisma by 2, Dexterity and Intelligence by 1";
+
+            _dexterityMod = _dexterityMod + 1;
+            _intelligenceMod = _intelligenceMod + 1;
+            _charismaMod = _charismaMod + 2;
+
+            this._selectedRace = selectedRace.Text;
+        }
+
+        //Radio Button for Halfling
+        private void halflingRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.M_Halfling2;
+            RadioButton selectedRace = (RadioButton)sender;
+            racialBonusTextBox.Text = "Increase Dexterity and Intelligence by 2, Strength is decreased by 1";
+
+            _strengthMod = _strengthMod - 1;
+            _dexterityMod = _dexterityMod + 2;
+            _intelligenceMod = _intelligenceMod + 2;
+
+            this._selectedRace = selectedRace.Text;
+        }
+
+        //Radio Button for Elf
+        private void elfRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.M_Elf1;
+            RadioButton selectedRace = (RadioButton)sender;
+            racialBonusTextBox.Text = "Increase Dexterity by 2, Charisma and Intelligence by 1";
+
+            _dexterityMod = _dexterityMod + 2;
+            _intelligenceMod = _intelligenceMod + 1;
+            _charismaMod = _charismaMod + 1;
+
+            this._selectedRace = selectedRace.Text;
+        }
+        /*
+        //Radio Button for Aasimar (Half-Angel, Half-Human)
+        private void aasimarRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            characterPictureBox.Image = Properties.Resources.[aasimar picture name];
+            RadioButton selectedRace = (RadioButton)sender;
+            racialBonusTextBox.Text = "Increase Charisma by 2, Wisdom and Constitution by 1";
+
+            _consitutionMod = _constitutionMod +1;
+            _wisdomMod = _wisdomMod +1;
+            _charismaMod = _charismaMod + 2;
+
+            this._selectedRace = selectedRace.Text;
+        }
+        */
+
     }
 }
